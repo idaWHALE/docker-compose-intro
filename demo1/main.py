4a2a1237647f59c2790941eb4d4e550ae75f1a3e
idaWHALE <eagidawhale@gmail.com>
2020-12-23 11:31:17 -0800

    Fix demo1 issues

--- demo1/main.py
+++ demo1/main.py
@@ -3,12 +3,11 @@ import redis
 import os
 from flask import Flask
 
-
 app = Flask(__name__)
 redis = redis.StrictRedis(host=os.environ.get('REDIS_HOST'),
-			  port=os.environ.get('REDIS_PORT'),
-			  db=os.environ.get('REDIS_DB'),
-			  password=os.environ.et('REDIS_PASSWORD')
+                          port=os.environ.get('REDIS_PORT'),
+                          db=os.environ.get('REDIS_DB'),
+                          password=os.environ.get('REDIS_PASSWORD'))
 
 
 @app.route('/')
@@ -20,14 +19,14 @@ def hello_world():
 def visitor():
     redis.incr('visitor')
     visitor_num = redis.get('visitor').decode("utf-8")
-    return "Visitor: %s" % (visitor_num)
+    return "Visitor: %s" % visitor_num
 
 
 @app.route('/visitor/reset')
 def reset_visitor():
     redis.set('visitor', 0)
     visitor_num = redis.get('visitor').decode("utf-8")
-    return "Visitor is reset to %s" % (visitor_num)
+    return "Visitor is reset to %s" % visitor_num
 
 
 if __name__ == '__main__':
